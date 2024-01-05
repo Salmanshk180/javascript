71c8901cfd62534894173af8919e0d0d7dabb48b
Salman Shaikh <salmanshaikh7118@gmail.com>
2024-01-05 16:55:29 +0530

    "third commit"

--- script.js
+++ script.js
@@ -496,3 +496,16 @@
 // var jane = new InformalPerson('jane','doe');
 
 // console.log(jane);
+
+// // <----- Strict Mode ------------>
+// "use strict";
+// var person;
+// persom = {};
+// console.log(persom);
+
+// function sum(a = 1, b = 2) {
+//     // SyntaxError: "use strict" not allowed in function with default parameter
+//     "use strict";
+//     return a + b;
+//   }
+  
